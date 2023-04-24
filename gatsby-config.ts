56702413e935e15b23e644cd065afb9df5c18cd3
Filip Lizoń <filip.lizon1@gmail.com>
2023-04-24 20:55:35 +0200

    add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,31 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Leezon Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
+    description: `Example project for the Gatsby Head API`,
+    image: `/icon.png`,
+    siteUrl: `https://www.leezon.pl`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "src/images/icon.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
