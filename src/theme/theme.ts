fee58098c957f5bba43c9f366c22d1b0f486867d
Filip Lizoń <filip.lizon1@gmail.com>
2023-04-27 18:35:51 +0200

    add custom theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+const theme = {
+  colors: {
+    dark: "#292929",
+    gray: "#4D4D4D",
+    lightgrey: "#898989",
+    white: "#FFFFFF",
+  },
+  fonts: {
+    primary: `'Prompt', sans-serif`,
+    secondary: `'Karla', sans-serif`,
+  },
+};
+
+export default extendTheme(theme); // or extendBaseTheme
