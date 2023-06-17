7238bbd187b2aade5dc6de7d9c05f8f9b173e152
Filip Lizoń <filip.lizon1@gmail.com>
2023-06-17 14:24:56 +0200

    style: 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,33 @@
 import * as React from "react";
-import { HeadFC, PageProps } from "gatsby";
+import { Flex, Text } from "@chakra-ui/react";
+import { HeadFC, Link, PageProps } from "gatsby";
 
 const NotFoundPage: React.FC<PageProps> = () => {
-  return <main></main>;
+  return (
+    <>
+      <Flex
+        as="section"
+        minH="100vh"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text fontSize={50} fontWeight="bold">
+          404
+        </Text>
+        <Text fontSize="2xl" fontWeight="bold" my={5}>
+          PAGE NOT FOUND
+        </Text>
+        <Text
+          transition=".2s"
+          borderBottom="1px solid transparent"
+          _hover={{ borderBottomColor: "black" }}
+        >
+          <Link to="/">GO BACK HOME</Link>
+        </Text>
+      </Flex>
+    </>
+  );
 };
 
 export default NotFoundPage;
