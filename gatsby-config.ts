ab8a3722be391100e52825f3ce98d9c086b7f719
Filip Lizoń <filip.lizon1@gmail.com>
2023-06-18 20:04:44 +0200

    refactor: change how fonts are loaded

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,26 +22,6 @@ const config: GatsbyConfig = {
         crossOrigin: `use-credentials`,
       },
     },
-    {
-      resolve: `gatsby-omni-font-loader`,
-      options: {
-        enableListener: true,
-        preconnect: [
-          `https://fonts.googleapis.com`,
-          `https://fonts.gstatic.com`,
-        ],
-        web: [
-          {
-            name: `Prompt`,
-            file: `https://fonts.googleapis.com/css2?family=Prompt:wght@600&display=swap`,
-          },
-          {
-            name: `Karla`,
-            file: `https://fonts.googleapis.com/css2?family=Karla:wght@300&display=swap`,
-          },
-        ],
-      },
-    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ gatsby-ssr.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <Helmet key="fonts">
+      <link
+        rel="preload"
+        href="/fonts/Karla.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+        key="karlaFont"
+      />
+      <link
+        rel="preload"
+        href="/fonts/Prompt.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+        key="promptFont"
+      />
+    </Helmet>,
+  ]);
+};
